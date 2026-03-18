ab4689042b3a1b25493c9ffc695b289dad28c189
Niraj Lamsal <nrjlamsal@gmail.com>
2026-03-18 10:29:42 -0700

    Used memcpy for manual copy and made argv last index null

--- ass.c
+++ ass.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_ARGS 1024
+#define MAX_INPUT 1024
+
 
 int makearg(char s[],char ***args){
     // if problem return -1 else return 1
@@ -10,49 +13,44 @@ int makearg(char s[],char ***args){
         return -1;
     }   
 
+
     // char **argv = *args; wrong sequence 
     //  argv =(char**)malloc(1024 *sizeof(char*));
 
-    char **argv =(char**)malloc(1024 *sizeof(char*));
+    char **argv =(char**)malloc(MAX_ARGS *sizeof(char*));
+    if(!argv) return -1;
     *args= argv;
-    char *buffer = (char *)malloc(1024 *sizeof(char));
+    char *buffer = (char *)malloc(MAX_INPUT *sizeof(char));
 
     int index = 0 ; //used during parse
     int aindex =0 ; // used for argv index
 
     for(int i=0 ; s[i]!='\0';i++){
-     if(s[i] == ' ' ){
+     if(s[i] == ' ' || s[i] == '\t'){
         if(index > 0){
             buffer[index] = '\0';
             argv[aindex] =(char*)malloc(sizeof(char)*(index+1));
-            for(int j=0; j < (index + 1) ; j++ ){
-                argv[aindex][j]= buffer[j];
-            }
-            index = 0;
+            memcpy(argv[aindex],buffer,index+1);
+           index = 0;
             aindex++;
 
+          if (aindex >= MAX_ARGS - 1) break;  // leave room for NULL
 
         }
      }else{
-     
         buffer[index++] = s[i];
      }
 
    }
 
-   if(index > 0){
-
+   if(index > 0 && aindex < MAX_ARGS -1){
         buffer[index] = '\0';
-
         argv[aindex] = (char *)malloc(index + 1);
-
-        for(int j=0; j<=index; j++)
-            argv[aindex][j] = buffer[j];
-
+        memcpy(argv[aindex],buffer,index+1);
         aindex++;
     }
 
-
+ argv[aindex] = NULL;
 free(buffer);
  return aindex;
 }
