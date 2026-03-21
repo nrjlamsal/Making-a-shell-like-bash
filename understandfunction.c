385f22845b7ce18a7fa1bd8b54836b200e813dce
Niraj Lamsal <nrjlamsal@gmail.com>
2026-03-21 09:24:52 -0700

    understand open, close,dup2 system calls

--- understandfunction.c
+++ understandfunction.c
@@ -3,12 +3,23 @@
 #include <string.h>
 #include <unistd.h> // fork,execvp
 #include <sys/wait.h> // wait
+#include <fcntl.h> // used for open(),close() and flags like O_WRONLY
 
 int main(){
 
-    char *argv[] ={"ls","-l",NULL};
-    // argv ={"cd",NULL};
+    // int fd=open("Testing.txt",O_WRONLY|O_CREAT|O_TRUNC,0666);
+    int fd=open("Testing.txt",O_RDONLY);
+    if(fd == -1){
+        exit(0);
+    }
+    dup2(fd,0);
+    char str[1024];
+    fgets(str,sizeof(str),stdin); // readds from file as we redirrected standard input to file
 
-    execvp("ls",argv);
-    return 0;
+    printf("%s\n",str);// ..pinnt the data we got from file
+
+    
+    close(fd);
+    
+   return 0;
 }
